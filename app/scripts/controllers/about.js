f55c75901febe28c6aeb995b3da55a314a0d877c
Peter Schabsky <peter.schabsky@fh-dortmund.de>
2015-12-03 11:08:55 +0100

    Initialisierung

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name privantAngularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the privantAngularApp
+ */
+angular.module('privantAngularApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
